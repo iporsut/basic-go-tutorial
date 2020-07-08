1b489eb222f2e24fd066687460d0f064bcb44a2a
Weerasak Chongnguluam <singpor@gmail.com>
2020-07-08 07:26:44 +0700

    Part 4

---
+++ 4-struct/main.go
@@ -0,0 +1,84 @@
+package main
+
+// type Profile struct {
+// 	Username          string
+// 	Firstname         string
+// 	Lastname          string
+// 	Age               int
+// 	EncryptedPassword string
+// }
+
+// type Profile struct {
+// 	Username          string
+// 	Firstname         string
+// 	Lastname          string
+// 	Age               int
+// 	EncryptedPassword string
+// 	FavoriteMovies    [5]string
+// 	LoggedIPHistories []string
+// 	MetaData          map[string]string
+// }
+
+func main() {
+	// var profile Profile
+	// fmt.Println(profile)
+	// fmt.Printf("%+v\n", profile)
+	// profile.Username = "por"
+	// profile.Firstname = "Weerasak"
+	// profile.Lastname = "Chongnguluam"
+	// profile.Age = 35
+	// profile.EncryptedPassword = "encyptedpass1234"
+	// fmt.Printf("%+v\n", profile)
+	// fmt.Println("Username:", profile.Username)
+	// fmt.Println("Firstname:", profile.Firstname)
+	// fmt.Println("Lastname:", profile.Lastname)
+	// fmt.Println("Age:", profile.Age)
+	// fmt.Println("EncryptedPassword:", profile.EncryptedPassword)
+	// profile := Profile{
+	// 	Username:  "por",
+	// 	Firstname: "Weerasak",
+	// 	Lastname:  "Chongnguluam",
+	// 	// Age:               35,
+	// 	// EncryptedPassword: "encyptedpass1234",
+	// }
+	// profile := Profile{"por", "Weerasak", "Chongnguluam", 35, "encyptedpass1234"}
+	// fmt.Println("Username:", profile.Username)
+	// fmt.Println("Firstname:", profile.Firstname)
+	// fmt.Println("Lastname:", profile.Lastname)
+	// fmt.Println("Age:", profile.Age)
+	// fmt.Println("EncryptedPassword:", profile.EncryptedPassword)
+
+	// profile := Profile{
+	// 	Username:          "por",
+	// 	Firstname:         "Weerasak",
+	// 	Lastname:          "Chongnguluam",
+	// 	Age:               35,
+	// 	EncryptedPassword: "encyptedpass1234",
+	// 	FavoriteMovies: [5]string{
+	// 		"Dark",
+	// 		"Game of Thrones",
+	// 		"A beautiful mind",
+	// 		"Avengers End game",
+	// 		"Iron Man",
+	// 	},
+	// 	LoggedIPHistories: []string{
+	// 		"192.168.1.1",
+	// 	},
+	// 	MetaData: map[string]string{
+	// 		"last-order": "Pizza",
+	// 	},
+	// }
+	// fmt.Println(profile)
+	// fmt.Println(profile.FavoriteMovies[0])
+	// profile.FavoriteMovies[0] = "Spider Man"
+	// fmt.Println(profile.FavoriteMovies[0])
+
+	// fmt.Println(profile.LoggedIPHistories[0])
+	// profile.LoggedIPHistories = append(profile.LoggedIPHistories, "127.0.0.1")
+	// fmt.Println(profile.LoggedIPHistories)
+
+	// fmt.Println(profile.MetaData["last-order"])
+	// profile.MetaData["last-order-price"] = "256"
+	// fmt.Println(profile.MetaData)
+
+}
